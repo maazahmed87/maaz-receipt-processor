0de03a77d94de14190693b53745e5e5ac9da8296
Maaz Ahmed <elfilosof@protonmail.com>
2025-02-03 15:03:52 -0800

    added graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"receipt-processor/internal/api/handler"
 	"receipt-processor/internal/domain/service"
 	"receipt-processor/internal/storage/memory"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -20,22 +24,48 @@ func setupHealthCheck(router *gin.Engine) {
 	})
 }
 
+func setupRouter(receiptHandler *handler.ReceiptHandler) *gin.Engine {
+	router := gin.Default()
+	setupHealthCheck(router)
+	router.POST("/receipts/process", receiptHandler.ProcessReceipt)
+	router.GET("/receipts/:id/points", receiptHandler.GetPoints)
+	return router
+}
+
 func main() {
 	// initialize the dependencies
 	storage := memory.NewMemoryStorage()
 	receiptService := service.NewReceiptService(storage)
 	receiptHandler := handler.NewReceiptHandler(receiptService)
 
-	// defined routes
-	// create a default gin router
-	router := gin.Default()
-	setupHealthCheck(router)
-	router.POST("/receipts/process", receiptHandler.ProcessReceipt)
-	router.GET("/receipts/:id/points", receiptHandler.GetPoints)
+	// setting up router
+	router := setupRouter(receiptHandler)
 
-	// starting the server
-	log.Println("Server starting on port 8080-----")
-	if err := router.Run(":8080"); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
+
+	// Starting server in a goroutine
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// using the interrupt signal to gracefully shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// Giving outstanding requests 5 seconds to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting...")
 }
